e0311a6fb139940703c2384e8b36064cfd0fdf04
Rasik <rasikraj01@gmail.com>
2017-08-04 01:39:02 +0530

    update and get shop controllers done

--- server/controllers/shop.controllers.js
+++ server/controllers/shop.controllers.js
@@ -17,11 +17,8 @@ module.exports = {
    },
    getShop(req, res){
       var id = req.params.id;
-      if (!ObjectId.isValid(id)) {
-        return res.status(404).send();
-      }
 
-      Shop.findOne({
+      Shop.findById({
          _id: id
       }).then((doc) => {
          if(!doc){
@@ -31,12 +28,18 @@ module.exports = {
       }).catch((e) => {
          res.status(400).send();
       });
+   },
+   updateShop(req, res){
+      var id = req.params.id;
+      var body = _.pick(req.body, ['address.street','address.city', 'address.zip','phone', 'description', 'closed', 'timeO', 'timeC', 'costL', 'costU', 'type']);
+
+      Shop.findOneAndUpdate({
+         _id:id
+      },{$set:body},{new:true})
+      .then((doc) => {res.send({doc});})
+      .catch((e) => {
+         res.status(400).send()
+      });
    }
-   // updateShop(req, res){
-   //    var id = req.params.id;
-   //    var body = _.pick(req.body);
-   //
-   //    Shop.findOneAndUpdate()
-   // }
 }
 //  ['address', 'phone', 'description', ]
--- server/server.js
+++ server/server.js
@@ -24,9 +24,9 @@ app.post('/shop', ShopController.createShop);
 //remove
 app.delete('/shop/:id', ShopController.deleteShop);
 //update
-//app.patch('/shop/:id', ShopController.updateShop);
+app.patch('/shop/:id', ShopController.updateShop);
 //delete
-app.get('shops/:id', ShopController.getShop);
+app.get('/shop/:id', ShopController.getShop);
 
 app.listen(PORT, () => {
    console.log(`Sever running on PORT ${PORT}`);
