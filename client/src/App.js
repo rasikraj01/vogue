82c5d983e098a76e60843d9d7f756b574e41110e
Rasik <rasikraj01@gmail.com>
2017-10-10 01:28:01 +0530

    login request

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,39 @@ class App extends Component {
      var  url = 'http://localhost:5000/shops'
       axios.get(url).then((shops) => {console.log(shops.data);});
   }
+  handleClickUser(e){
+     e.preventDefault();
+     var url='http://localhost:5000/user/me';
+     axios.get(url).then((body) => {console.log(body)});
+ }
+ handleSubmitLogin(e){
+    e.preventDefault();
+    var body = {email:this.refs.email_login.value, password:this.refs.password_login.value}
+    console.log(body)
+    var url='http://localhost:5000/user/login'
+    axios.post(url, body).then((body) => {console.log(body); console.log('logged in');}).catch((e) => {console.log(e)});
+}
+handleShop(e){
+   var body ={
+	"address":{
+      "street":
+         "1",
+      "city":"delhimum"
+      ,
+      "zip":"000999"
+   },
+   "phone":"1221280",
+   "description":"good shop",
+   "closed":"Sunday",
+   "timeO":"0800",
+   "timeC":"1800",
+   "costL":"111",
+   "costU":"2222",
+   "type":"kids fashion"
+}
+var url='http://localhost:5000/shop/'
+axios.post(url, body).then(body => {console.log(body);}).catch((e) => {console.log(e);})
+}
   render() {
     return (
       <div className="App">
@@ -28,6 +61,14 @@ class App extends Component {
            <button type="submit">submit</button>
         </form>
           <p onClick={this.handleClick}>shop</p>
+          <p onClick={this.handleClickUser}>User</p>
+          <p>login user</p>
+         <form action="http://localhost:5000/user/login" method="POST" onSubmit={this.handleSubmitLogin.bind(this)}>
+            <input type="text" ref="email_login" placeholder="email"/>
+            <input type="password" ref="password_login" placeholder="password"/>
+            <button type="submit">submit</button>
+         </form>
+      <p onClick={this.handleShop}>make a shop</p>
       </div>
     );
   }
