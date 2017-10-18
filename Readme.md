2c0ab23aeaa76619235503a08ec0ce0cfadbe210
Rasik <rasikraj01@gmail.com>
2017-10-18 10:16:05 +0530

    added readme

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# vogue
+(WIP)This is a MERN stack based app.
+
+
+To Start the servers:
+
+<pre><code>
+cd vogue
+npm install
+cd client
+npm install
+cd ..
+npm run dev
+</code></pre>
+
+Also in another termianl tab run :
+
+<code>mongod</code>
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
 
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-   res.setHeader('Access-Control-Allow-Headers', 'x-access-token,content-type');
+   res.setHeader('Access-Control-Allow-Headers', 'x-auth, content-type');
 
    res.setHeader('Access-Control-Allow-Credentials', true);
 
